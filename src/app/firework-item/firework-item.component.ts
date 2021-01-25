eb7a7c25aadbdbdd883b0011ac84e3e98de58483
julien <julien-9871@hotmail.fr>
2021-01-25 16:40:21 +0100

    update

--- src/app/firework-item/firework-item.component.ts
+++ src/app/firework-item/firework-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Firework } from '../classes/Firework';
 
 @Component({
   selector: 'app-firework-item',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./firework-item.component.css']
 })
 export class FireworkItemComponent implements OnInit {
-
+  msg: String = "item feu d'artifice";
+  @Input() city: String
+  @Input() address: String
+  @Input() price: Int16Array
+  @Input() handicapAccess: boolean
+  @Input() duration: String
+  @Input() crowed: String
   constructor() { }
 
   ngOnInit(): void {
